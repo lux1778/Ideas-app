f52d3486b21f88c1f1b5c1e2fc7484a763e6dede
dawid <dawidjasinski1778@gmail.com>
2022-12-08 21:50:01 +0100

    Utworzenie modeli aplikacji

--- ideas/ideasapp/models.py
+++ ideas/ideasapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+idea_choices = (
+    ('pending', 'Waiting for review'),
+    ('accepted', 'Accepted'),
+    ('done', 'Done'),
+    ('rejected', 'Rejected'),
+)
+
+
 # Create your models here.
+class Ideas(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(choices=idea_choices, default='pending')
+
+
+class Vote(models.Model):
+    idea = models.ForeignKey(Ideas, on_delete=models.CASCADE())
+    reason = models.TextField()
